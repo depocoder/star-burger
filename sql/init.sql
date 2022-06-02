b35932e8bf53d1bb22da24a90425f377c0b72f02
depocoder <minecrafter0079@yandex.ru>
2022-06-02 23:39:09 +0300

    fix typo

--- sql/init.sql
+++ sql/init.sql
@@ -2,4 +2,4 @@ CREATE DATABASE star_burger;
 CREATE USER some_user WITH PASSWORD 'P@ssw0rd';
 GRANT ALL ON DATABASE star_burger TO some_user;
 ALTER USER some_user CREATEDB;
-postgres=# update pg_database set encoding = pg_char_to_encoding('UTF8') where datname = 'star_burger';
+update pg_database set encoding = pg_char_to_encoding('UTF8') where datname = 'star_burger';
