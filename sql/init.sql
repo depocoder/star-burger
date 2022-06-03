25aa797c2890c4129677f506dfe7982cd86a3b2b
depocoder <minecrafter0079@yandex.ru>
2022-06-03 12:52:12 +0300

    update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE star_burger;
 CREATE USER some_user WITH PASSWORD 'P@ssw0rd';
-GRANT ALL ON DATABASE star_burger TO some_user;
 ALTER USER some_user CREATEDB;
+ALTER ROLE some_user SET client_encoding TO 'utf8';
+GRANT ALL ON DATABASE star_burger TO some_user;
 update pg_database set encoding = pg_char_to_encoding('UTF8') where datname = 'star_burger';
