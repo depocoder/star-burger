e78eaf6fc264fc544c1f35c356c9ceb74542ab45
depocoder <minecrafter0079@yandex.ru>
2022-06-04 15:20:57 +0300

    delete useless code from init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE star_burger;
 CREATE USER some_user WITH PASSWORD 'P@ssw0rd';
 ALTER USER some_user CREATEDB;
-ALTER ROLE some_user SET client_encoding TO 'utf8';
 GRANT ALL ON DATABASE star_burger TO some_user;
 update pg_database set encoding = pg_char_to_encoding('UTF8') where datname = 'star_burger';
