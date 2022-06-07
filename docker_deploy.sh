10088a7445c0d9af4a977f1144fea0d6ca37e610
depocoder <minecrafter0079@yandex.ru>
2022-06-07 22:34:16 +0300

    refactor docker_deploy.sh

---
+++ docker_deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+echo 'load .env'
+source .env
+echo 'start git pull'
+git pull
+echo 'build images'
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
+
+echo 'restart containers'
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml restart
+echo 'migrate'
+docker exec star_burger_web "python" "manage.py" "migrate" "--no-input"
+
+COMMIT_HAST=$(git rev-parse HEAD)
+curl -H "X-Rollbar-Access-Token: $ROLLBAR_ACCESS_TOKEN" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d "{\"environment\": \"$ROLLBAR_ENVIRONMENT_NAME\", \"revision\": \"$COMMIT_HAST\"  , \"status\": \"succeeded\"}"
+echo 'deploy completed'
