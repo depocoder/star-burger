c6ae55ab0d31a78d023b799083b47647c5fddfe0
depocoder <minecrafter0079@yandex.ru>
2022-06-30 22:31:33 +0300

    delete: remove init.sql

--- sql/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE star_burger;
-CREATE USER some_user WITH PASSWORD 'P@ssw0rd';
-ALTER USER some_user CREATEDB;
-GRANT ALL ON DATABASE star_burger TO some_user;
-update pg_database set encoding = pg_char_to_encoding('UTF8') where datname = 'star_burger';
