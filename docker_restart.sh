0f6d090588ce8b5bc226d091f8b128b02cdd3ffe
depocoder <minecrafter0079@yandex.ru>
2022-07-08 15:10:24 +0300

    refactor: docker_restart.sh move line order

--- docker_restart.sh
+++ docker_restart.sh
@@ -9,10 +9,10 @@ docker-compose -f docker-compose.production.yml build web db
 echo 'restart containers'
 docker-compose -f docker-compose.production.yml down
 docker-compose -f docker-compose.production.yml up -d
-echo 'run tests'
-docker exec star_burger_web "python" "manage.py" "test"
 echo 'migrate'
 docker exec star_burger_web "python" "manage.py" "migrate" "--no-input"
+echo 'run tests'
+docker exec star_burger_web "python" "manage.py" "test"
 COMMIT_HAST=$(git rev-parse HEAD)
 curl -H "X-Rollbar-Access-Token: $ROLLBAR_ACCESS_TOKEN" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d "{\"environment\": \"$ROLLBAR_ENVIRONMENT_NAME\", \"revision\": \"$COMMIT_HAST\"  , \"status\": \"succeeded\"}"
 echo 'deploy completed'
