18b4a15cfb64ca872c7696093efaf26bc2b558ea
depocoder <minecrafter0079@yandex.ru>
2022-07-22 15:52:32 +0300

    refactor: fix tests run

--- redeploy.sh
+++ redeploy.sh
@@ -9,7 +9,7 @@ docker build frontend/ -t star_burger_frontend
 docker-compose -f docker-compose.production.web.yml build web
 echo 'run tests'
 # for using manage.py we should use SECRET_KEY
-docker run star_burger_web SECRET_KEY=NOT_IMPORTANT_VALUE python manage.py test
+docker run --network host --env-file ./.env star_burger_web python manage.py test
 echo 'restart web'
 docker-compose -f docker-compose.production.web.yml down
 docker-compose -f docker-compose.production.web.yml up -d
