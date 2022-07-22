5a4422a731cc67e475f1d1f460390fd26bebcc81
depocoder <minecrafter0079@yandex.ru>
2022-07-22 15:19:36 +0300

    refactor: move copy static down

--- redeploy.sh
+++ redeploy.sh
@@ -7,9 +7,6 @@ git pull
 echo 'build images'
 docker build frontend/ -t star_burger_frontend
 docker-compose -f docker-compose.production.web.yml build web
-echo 'copy static'
-docker cp star_burger_web:/code/staticfiles/. ./static/
-docker cp star_burger_frontend:/frontend/bundles/. ./static/
 echo 'run tests'
 docker run star_burger_web python manage.py test
 echo 'restart web'
@@ -17,6 +14,9 @@ docker-compose -f docker-compose.production.web.yml down
 docker-compose -f docker-compose.production.web.yml up -d
 echo 'migrate'
 docker exec star_burger_web python manage.py migrate --no-input
+echo 'copy static'
+docker cp star_burger_web:/code/staticfiles/. ./static/
+docker cp star_burger_frontend:/frontend/bundles/. ./static/
 
 echo 'send deploy info to rollbar'
 COMMIT_HAST=$(git rev-parse HEAD)
